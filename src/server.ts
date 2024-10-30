a8004c95173f5799284e06defb1f224f7895c88b
Nik Lakhani <mr.niklakhani@gmail.com>
2024-10-30 00:15:23 -0400

    add express + its settings

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log(process.env.PORT)
+import app from './app'
+import { Config } from './config'
+
+const startServer = () => {
+  const PORT = Config.PORT
+  try {
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+startServer()
