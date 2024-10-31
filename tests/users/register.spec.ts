b45fab25034d358313391eee9aa53bb19afb0ca6
Nik Lakhani <mr.niklakhani@gmail.com>
2024-10-31 03:13:15 -0400

    add test for trim email

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -179,4 +179,25 @@ describe('POST /auth/register', () => {
       expect(users).toHaveLength(0)
     })
   })
+
+  describe('Fields are not in proper format', () => {
+    it('should trim the email field', async () => {
+      // Arrange
+      const userData = {
+        firstName: 'John',
+        lastName: 'D',
+        email: ' john.d@localhost ',
+        password: 'secret',
+      }
+
+      // Act
+      await request(app).post('/auth/register').send(userData)
+
+      // Assert
+      const userRepository = connection.getRepository(User)
+      const users = await userRepository.find()
+      const user = users[0]
+      expect(user.email).toBe('john.d@localhost')
+    })
+  })
 })
