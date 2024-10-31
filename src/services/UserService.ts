e33c3cffd0330e88def576e0ce14ce4680932966
Nik Lakhani <mr.niklakhani@gmail.com>
2024-10-31 02:33:55 -0400

    make email field unique + add test

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -9,6 +9,12 @@ export class UserService {
   constructor(private userRepository: Repository<User>) {}
 
   async create({ firstName, lastName, email, password }: UserData) {
+    const user = await this.userRepository.findOne({ where: { email: email } })
+    if (user) {
+      const error = createHttpError(400, 'Email already exists!')
+      throw error
+    }
+
     // Hash the password
     const saltRounds = 10
     const hashedPassword = await bcrypt.hash(password, saltRounds)
