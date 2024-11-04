c12e3d3b0e10a7687d2e497fdda96a9c23cbb57d
Nik Lakhani <mr.niklakhani@gmail.com>
2024-11-04 00:44:21 -0500

    check if user is authenticated while creating tenant

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -1,8 +1,14 @@
-import express, { NextFunction, RequestHandler, Response } from 'express'
+import express, {
+  NextFunction,
+  Request,
+  RequestHandler,
+  Response,
+} from 'express'
 import { AppDataSource } from '../config/data-source'
 import logger from '../config/logger'
 import { TenantController } from '../controllers/TenantController'
 import { Tenant } from '../entity/Tenant'
+import authenticate from '../middlewares/authenticate'
 import { TenantService } from '../services/TenantService'
 import { CreateTenantRequest } from '../types'
 
@@ -12,12 +18,14 @@ const tenantRepo = AppDataSource.getRepository(Tenant)
 const tenantService = new TenantService(tenantRepo)
 const tenantController = new TenantController(tenantService, logger)
 
-router.post('/', ((
-  req: CreateTenantRequest,
-  res: Response,
-  next: NextFunction,
-) => {
-  return tenantController.create(req, res, next)
-}) as RequestHandler)
+router.post(
+  '/',
+  ((req: Request, res: Response, next: NextFunction) => {
+    return authenticate(req, res, next)
+  }) as RequestHandler,
+  (req: CreateTenantRequest, res: Response, next: NextFunction) => {
+    return tenantController.create(req, res, next)
+  },
+)
 
 export default router
--- tests/tenants/create.spec.ts
+++ tests/tenants/create.spec.ts
@@ -1,20 +1,35 @@
+import createJWKMock from 'mock-jwks'
 import request from 'supertest'
 import { DataSource } from 'typeorm'
 import app from '../../src/app'
 import { AppDataSource } from '../../src/config/data-source'
+import { Roles } from '../../src/constants'
 import { Tenant } from '../../src/entity/Tenant'
 
 describe('POST /tenants', () => {
   let connection: DataSource
+  let jwks: ReturnType<typeof createJWKMock>
+  let adminToken: string
 
   beforeAll(async () => {
+    jwks = createJWKMock('http://localhost:5501')
     connection = await AppDataSource.initialize()
   })
 
   beforeEach(async () => {
+    jwks.start()
     // Truncate database
     await connection.dropDatabase()
     await connection.synchronize()
+
+    adminToken = jwks.token({
+      sub: '1',
+      role: Roles.ADMIN,
+    })
+  })
+
+  afterEach(async () => {
+    jwks.stop()
   })
 
   afterAll(async () => {
@@ -24,13 +39,19 @@ describe('POST /tenants', () => {
   describe('Given all fields', () => {
     it('should return a 201 status code', async () => {
       const tenantData = { name: 'Tenant Name', address: 'Tenant Address' }
-      const response = await request(app).post('/tenants').send(tenantData)
+      const response = await request(app)
+        .post('/tenants')
+        .set('Cookie', [`accessToken=${adminToken}`])
+        .send(tenantData)
       expect(response.statusCode).toBe(201)
     })
 
     it('should create a tenant in the database', async () => {
       const tenantData = { name: 'Tenant Name', address: 'Tenant Address' }
-      const response = await request(app).post('/tenants').send(tenantData)
+      const response = await request(app)
+        .post('/tenants')
+        .set('Cookie', [`accessToken=${adminToken}`])
+        .send(tenantData)
 
       const tenantRepo = connection.getRepository(Tenant)
       const tenants = await tenantRepo.find()
@@ -38,6 +59,17 @@ describe('POST /tenants', () => {
       expect(tenants).toHaveLength(1)
       expect(tenants[0].name).toBe(tenantData.name)
       expect(tenants[0].address).toBe(tenantData.address)
+    })
+
+    it('should 401 if user is not authenticated', async () => {
+      const tenantData = { name: 'Tenant Name', address: 'Tenant Address' }
+      const response = await request(app).post('/tenants').send(tenantData)
+      expect(response.statusCode).toBe(401)
+
+      const tenantRepo = connection.getRepository(Tenant)
+      const tenants = await tenantRepo.find()
+
+      expect(tenants).toHaveLength(0)
     })
   })
 })
