2e494cdac89d8349932599956530ddd3f52c9638
Nik Lakhani <mr.niklakhani@gmail.com>
2024-11-03 23:39:07 -0500

    add create tenant endpoint

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -1,9 +1,23 @@
-import express, { Request, RequestHandler, Response } from 'express'
+import express, { NextFunction, RequestHandler, Response } from 'express'
+import { AppDataSource } from '../config/data-source'
+import logger from '../config/logger'
+import { TenantController } from '../controllers/TenantController'
+import { Tenant } from '../entity/Tenant'
+import { TenantService } from '../services/TenantService'
+import { CreateTenantRequest } from '../types'
 
 const router = express.Router()
 
-router.post('/', ((req: Request, res: Response) => {
-  res.status(201).json({})
+const tenantRepo = AppDataSource.getRepository(Tenant)
+const tenantService = new TenantService(tenantRepo)
+const tenantController = new TenantController(tenantService, logger)
+
+router.post('/', ((
+  req: CreateTenantRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  return tenantController.create(req, res, next)
 }) as RequestHandler)
 
 export default router
--- tests/tenants/create.spec.ts
+++ tests/tenants/create.spec.ts
@@ -2,6 +2,7 @@ import request from 'supertest'
 import { DataSource } from 'typeorm'
 import app from '../../src/app'
 import { AppDataSource } from '../../src/config/data-source'
+import { Tenant } from '../../src/entity/Tenant'
 
 describe('POST /tenants', () => {
   let connection: DataSource
@@ -25,6 +26,18 @@ describe('POST /tenants', () => {
       const tenantData = { name: 'Tenant Name', address: 'Tenant Address' }
       const response = await request(app).post('/tenants').send(tenantData)
       expect(response.statusCode).toBe(201)
+    })
+
+    it('should create a tenant in the database', async () => {
+      const tenantData = { name: 'Tenant Name', address: 'Tenant Address' }
+      const response = await request(app).post('/tenants').send(tenantData)
+
+      const tenantRepo = connection.getRepository(Tenant)
+      const tenants = await tenantRepo.find()
+
+      expect(tenants).toHaveLength(1)
+      expect(tenants[0].name).toBe(tenantData.name)
+      expect(tenants[0].address).toBe(tenantData.address)
     })
   })
 })
